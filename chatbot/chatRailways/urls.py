f4bceae9508f0da82f1f66a06675f8d5ab0f0c64
Your Name <lavanyachavali03@gmail.com>
2025-02-12 19:28:23 +0530

    Lav update

--- chatbot/chatRailways/urls.py
+++ chatbot/chatRailways/urls.py
@@ -1,18 +1,3 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('', views.render_webpage, name='chatbot'),
-#     path('speech/', views.receive_audio, name='speech'),
-#     path('text-to-speech/', views.text_to_speech, name='text_to_speech'),
-#     path('chatbot/', views.chatbot_response, name='chatbot_response'),
-# ]
-
-
-
-
-
-
 from django.urls import path
 from . import views
 
